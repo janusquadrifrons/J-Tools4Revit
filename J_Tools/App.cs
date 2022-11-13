3a85c0c35baab3c6c791ea8fc075ef722c3b2bdb
janusquadrifrons <52275596+janusquadrifrons@users.noreply.github.com>
2022-11-13 13:02:56 +0300

    Add files via upload

---
+++ J_Tools/App.cs
@@ -0,0 +1,81 @@
+/// 211002 : buton iconlarý çýkmýyor.
+
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+#endregion
+
+namespace J_Tools
+{
+    class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            string tabname = "J Tools";
+            string panelname = "Janus Tools Panel";
+
+            // --- create the tab
+            a.CreateRibbonTab(tabname);
+            // --- create the panel
+            var panel = a.CreateRibbonPanel(tabname, panelname);
+            // --- create buttons
+            var button1 = new PushButtonData("J Tools button1", "J_Trim", Assembly.GetExecutingAssembly().Location, "J_Tools.Command1");
+            button1.ToolTip = "Trim the ACAD Way...";
+            button1.LongDescription = "Trim lines as in AutoCad...";
+            // --- create stacked buttons
+            var button2 = new PushButtonData("J Tools button2", "MatchProp", Assembly.GetExecutingAssembly().Location, "J_Tools.Command2");
+            button2.ToolTip = "Quick Style Painter...";
+            button2.LongDescription = "Match style properties of entities as in ArchiCad...";
+
+            var button3 = new PushButtonData("J Tools button3", "SelectSimilar", Assembly.GetExecutingAssembly().Location, "J_Tools.Command3");
+            button3.ToolTip = "Sibling Selector";
+            button3.LongDescription = "Select similar entities with similar styles as in AutoCad...";
+
+            var button4 = new PushButtonData("J Tools button4", "Parameters", Assembly.GetExecutingAssembly().Location, "J_Tools.Command4");
+            button4.ToolTip = "ooo";
+            button4.LongDescription = "xxx";
+
+            var button6 = new PushButtonData("J Tools button6", "QSelect", Assembly.GetExecutingAssembly().Location, "J_Tools.Command6");
+            button6.ToolTip = "ooo";
+            button6.LongDescription = "xxx";
+
+
+            // --- add buttons to the panel
+            PushButton btn1 = panel.AddItem(button1) as PushButton;
+            //panel.AddStackedItems(button2, button3);
+            IList<RibbonItem> stackedButtons = panel.AddStackedItems(button2, button3, button6);
+            //PushButton btn4 = panel.AddItem(button4) as PushButton;
+
+            // --- creating icons
+            BitmapImage icon_button1 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/trim-100c.png"));
+            //BitmapImage icon_button1 = new BitmapImage(new Uri("Resources/trim-100c.png", UriKind.Relative));
+            //BitmapImage icon_button1 = new BitmapImage(new Uri(@"C:\E:\98 cs\REVIT API\J_Tools\J_Tools\Resources\trim-100c.png"));
+            BitmapImage icon_button2 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/injection-12.png"));
+            BitmapImage icon_button3 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/multiple-12.png"));
+            // --- assigning icons
+            //button1.LargeImage = icon_button1;
+            btn1.LargeImage = icon_button1;
+            button2.Image = icon_button2;
+            button2.LargeImage = icon_button2;
+            button3.Image = icon_button3;
+            button3.LargeImage = icon_button3;
+            
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ J_Tools/Command6.cs
@@ -0,0 +1,52 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+using System.Text;
+using J_Tools.Extensions.SelectionExtensions;
+#endregion
+
+// Master Selection Filter
+
+namespace J_Tools
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    class Command6 : IExternalCommand
+    {
+        
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            // Declerations : Accessing Revit UI and corresponding databases
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+
+            var references = uidoc.Selection.PickObjects(
+                ObjectType.Element, new ElementsSelectionFilter(
+                    e => e is Dimension)); // --- via this Lambda expression will allow selectiong objects that only ones classified at ElementSelectionFilter
+            
+            /*
+            var references_2 = uidoc.Selection.PickObjects(
+                ObjectType.PointOnElement, new ElementsSelectionFilter(
+                    e => e is Dimension)); // --- via this Lambda expression will allow selectiong objects that only ones classified at ElementSelectionFilter
+            */
+
+            return Result.Succeeded;
+        }
+
+    }
+}
