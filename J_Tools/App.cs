2da0664afc20bb449c5d2fa04c0a3aba432576d9
janusquadrifrons <52275596+janusquadrifrons@users.noreply.github.com>
2023-07-13 14:34:35 +0300

    ViewToFace Update

--- J_Tools/App.cs
+++ J_Tools/App.cs
@@ -1,4 +1,4 @@
-/// 211002 : buton iconlarý çýkmýyor.
+/// 230707 : TODO : button9 icon
 
 #region Namespaces
 using System;
@@ -22,17 +22,19 @@ namespace J_Tools
             string tabname = "J Tools";
             string panelname = "Janus Tools Panel";
 
-            // --- create the tab
+            // --- Create the tab
             a.CreateRibbonTab(tabname);
-            // --- create the panel
+            // --- Create the panel
             var panel = a.CreateRibbonPanel(tabname, panelname);
-            // --- create buttons
+
+            #region Buttons
+            // --- Create buttons
             var button1 = new PushButtonData("J Tools button1", "J_Trim", Assembly.GetExecutingAssembly().Location, "J_Tools.Command7");
-            button1.ToolTip = "Trim the ACAD Way...";
+            button1.ToolTip = "Trim the ACAD Way";
             button1.LongDescription = "Trim lines as in AutoCad...";
-            // --- create stacked buttons
-            var button2 = new PushButtonData("J Tools button2", "MatchProp", Assembly.GetExecutingAssembly().Location, "J_Tools.Command2");
-            button2.ToolTip = "Quick Style Painter...";
+            // --- Create stacked buttons
+            var button2 = new PushButtonData("J Tools button2", "J_MatchProp", Assembly.GetExecutingAssembly().Location, "J_Tools.Command2");
+            button2.ToolTip = "Quick Style Painter";
             button2.LongDescription = "Match style properties of entities as in ArchiCad...";
 
             var button3 = new PushButtonData("J Tools button3", "J_SelectSimilar", Assembly.GetExecutingAssembly().Location, "J_Tools.Command3");
@@ -41,19 +43,19 @@ namespace J_Tools
 
             var button4 = new PushButtonData("J Tools button4", "J_SelDel_Pt", Assembly.GetExecutingAssembly().Location, "J_Tools.Command8_J_SelDel_Pt");
             button4.ToolTip = "ooo";
-            button4.LongDescription = "xxx";
+            // button4.LongDescription = "xxx";
 
             var button6 = new PushButtonData("J Tools button6", "J_SelDel_Dim", Assembly.GetExecutingAssembly().Location, "J_Tools.Command6_J_SelDel_Dim");
-            button6.ToolTip = "ooo";
-            button6.LongDescription = "xxx";
+            button6.ToolTip = "Delete dimensions in selected area";
+            // button6.LongDescription = "xxx";
 
-            var button_09 = new PushButtonData("J Tools button_09", "J_ViewToFace", Assembly.GetExecutingAssembly().Location, "J_Tools.Command_09");
+            var button_09 = new PushButtonData("J Tools button_09", "J_ViewToFace", Assembly.GetExecutingAssembly().Location, "J_Tools.Command_09_ViewToFace");
             button_09.ToolTip = "Aligns 3D view to a selected planar face.";
-            button_09.LongDescription = "xxx";
+            // button_09.LongDescription = "xxx";
 
-            var button_00_deneme = new PushButtonData("J Tools Deneme", "J_Deneme", Assembly.GetExecutingAssembly().Location, "J_Tools.Command_00_Deneme");
-
-
+            // --- Drafting 
+            // var button_00_deneme = new PushButtonData("J Tools Deneme", "J_Deneme", Assembly.GetExecutingAssembly().Location, "J_Tools.Command_00_Deneme");
+            #endregion
 
 
             // --- add buttons to the panel
@@ -70,16 +72,11 @@ namespace J_Tools
             //BitmapImage icon_button1 = new BitmapImage(new Uri(@"C:\E:\98 cs\REVIT API\J_Tools\J_Tools\Resources\trim-100c.png"));
             BitmapImage icon_button2 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/injection-12.png"));
             BitmapImage icon_button3 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/multiple-12.png"));
-            BitmapImage icon_button9 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/zoom-32.png"));
-            // --- assigning icons
-            //button1.LargeImage = icon_button1;
-            btn1.LargeImage = icon_button1;
-            button2.Image = icon_button2;
-            button2.LargeImage = icon_button2;
-            button3.Image = icon_button3;
-            button3.LargeImage = icon_button3;
-            button_09.LargeImage = icon_button9;
+            BitmapImage icon_button9 = new BitmapImage(new Uri("pack://application:,,,/J_Tools;component/Resources/icons8-rotate-32.png"));
             
+            // --- assigning icons
+            btn1.LargeImage = icon_button1;
+            btn9.LargeImage = icon_button9;
 
             return Result.Succeeded;
         }
